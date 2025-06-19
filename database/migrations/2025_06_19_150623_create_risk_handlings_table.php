246f5f8d88aea291825eca8cbce19df20d83ff19
pongkowulu114117 <putrapongkowulu@gmail.com>
2025-06-20 02:13:51 +0700

    memperbarui field table rsk-hendle yaitu: risk-approved, messages dan ttd

--- database/migrations/2025_06_19_150623_create_risk_handlings_table.php
+++ database/migrations/2025_06_19_150623_create_risk_handlings_table.php
@@ -16,10 +16,17 @@ return new class extends Migration
             $table->uuid('risk_id');
             $table->unsignedBigInteger('handled_by');
             $table->enum('effectiveness', ['TE', 'KE', 'E']);
+            
+            $table->boolean('is_approved')->nullable(); 
+            $table->text('review_notes')->nullable();  
+            $table->unsignedBigInteger('reviewed_by')->nullable();
+            $table->string('approval_signature')->nullable();
+
             $table->timestamps();
 
             $table->foreign('risk_id')->references('id')->on('risks')->onDelete('cascade');
             $table->foreign('handled_by')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('reviewed_by')->references('id')->on('users')->onDelete('set null');
         });
     }
 
